37cfea15fc8be2fb5f089a54cb09cea3e6681cee
Cesar Rodriguez <cesar.sgr@gmail.com>
2024-02-27 19:36:12 -0300

    Add new methods RemoveLast, Prepend and get on DoublyLInkedList

--- src/com/datastructure/doublelinkedlist/contructor/DoublyLinkedList.java
+++ src/com/datastructure/doublelinkedlist/contructor/DoublyLinkedList.java
@@ -13,16 +13,85 @@ public class DoublyLinkedList {
     }
 
     public void append(int value){
+        Node newNode = new Node(value);
         if(head == null){
-            return;
+            head = newNode;
+            tail = newNode;
+        }else {
+            newNode.next = tail.next;
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        length++;
+
+    }
+
+    public Node removeLast(){
+        if(length == 0){
+            return null;
+        }
+        Node temp = tail;
+        tail = tail.prev;
+        tail.next = temp.next;
+        temp.prev = null;
+        length--;
+
+        if(length == 0){
+            head = null;
+            tail = null;
         }
 
+        return temp;
+    }
+
+    public void prepend(int value){
         Node newNode = new Node(value);
-        newNode.next = tail.next;
-        tail.next = newNode;
-        newNode.prev = tail;
-        tail = newNode;
+        if (length == 0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            newNode.prev = head.prev;
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
         length++;
+    }
+
+    public Node removeFirst(){
+        Node temp = head;
+        if(length == 0){
+            return null;
+        }
+        if(length-1 == 0){
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public Node get(int index) {
+        if(index < 0 || index >= length){
+            return null;
+        }
+        Node temp = head;
+        if(index > length/2 ){
+            temp = tail;
+            for(int i = (length-1); i > index; i--){
+                temp = temp.prev;
+            }
+            return temp;
+        }
+        for(int i = 0; i < index; i++){
+            temp = temp.next;
+        }
+        return temp;
 
     }
 
--- src/com/datastructure/doublelinkedlist/contructor/Main.java
+++ src/com/datastructure/doublelinkedlist/contructor/Main.java
@@ -2,6 +2,8 @@ package com.datastructure.doublelinkedlist.contructor;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Start a new Contructor =========== \n");
         DoublyLinkedList myDLL = new DoublyLinkedList(7);
 
         myDLL.getHead();
@@ -9,9 +11,109 @@ public class Main {
         myDLL.getLength();
 
         myDLL.print();
-
-
+        System.out.println("");
+        System.out.println("Append a value ==================== \n");
         myDLL.append(6);
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.print();
+
+        System.out.println("");
+        System.out.println("Remove Last value ================== \n");
+
+        Node nodeRemoved = myDLL.removeLast();
+        System.out.println("Node Removed: " + nodeRemoved);
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.print();
+
+
+        System.out.println("");
+        System.out.println("Prepend value ================== \n");
+
+        myDLL.prepend(8);
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.print();
+
+        System.out.println("");
+        System.out.println("Remove First value ================== \n");
+        myDLL.removeFirst();
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.print();
+
+        System.out.println("");
+        System.out.println("Remove First value ================== \n");
+        myDLL.removeFirst();
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.print();
+
+        System.out.println("");
+        System.out.println("Append value ================== \n");
+        myDLL.append(0);
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.print();
+        System.out.println("");
+        System.out.println("Append value ================== \n");
+        myDLL.append(1);
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.print();
+        System.out.println("");
+        System.out.println("Append value ================== \n");
+        myDLL.append(2);
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.print();
+        System.out.println("");
+        System.out.println("Append value ================== \n");
+        myDLL.append(3);
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.print();
+
+        System.out.println("");
+        System.out.println("RAppend value ================== \n");
+        myDLL.append(4);
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.print();
+
+        System.out.println("");
+        System.out.println("GET Greater then half length value ================== \n");
+        System.out.println(myDLL.get(4).getValue());
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+
+        myDLL.print();
+
+        System.out.println("");
+        System.out.println("GET Less then half length value ================== \n");
+        System.out.println(myDLL.get(0).getValue());
         myDLL.getHead();
         myDLL.getTail();
         myDLL.getLength();
